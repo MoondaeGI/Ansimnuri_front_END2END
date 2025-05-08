d853316a386ac0ec6a1f17d1cd5f1ad231048e93
test <test@test.com>
2025-05-08 12:23:06 +0900

    update bootstrap

--- src/App.js
+++ src/App.js
@@ -3,30 +3,75 @@ import Main from './page/main/Main'
 import Board from './page/board/Board'
 import {Login, MyPage, SignIn} from './page/member'
 import {Routes, Route} from 'react-router-dom'
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
+import {Container, Row, Col, Navbar, Nav} from 'react-bootstrap'
+import {useState} from 'react'
 
 function App() {
+  const [expanded, setExpanded] = useState(false)
+
   return (
-    <Container className="App" fluid>
-      <Row>
-        <Col className="">헤더</Col>
-      </Row>
-      <Row>
-        <main>
-          <Routes>
-            <Route path="/" element={<Main />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/mypage" element={<MyPage />} />
-            <Route path="/notice" element={<Board />} />
-            <Route path="/qna" element={<Board />} />
-          </Routes>
-        </main>
-      </Row>
-      <Row>
-        <Col>푸터</Col>
-      </Row>
+    <Container fluid className="App">
+      {/* Header */}
+      <Navbar
+        expand="md"
+        bg="light"
+        className="py-3"
+        expanded={expanded}
+        onToggle={setExpanded}>
+        <Container>
+          <Navbar.Brand href="/">안심누리</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="/download">앱 다운로드</Nav.Link>
+              <Nav.Link href="/notice">공지사항</Nav.Link>
+              <Nav.Link href="/qna">QNA</Nav.Link>
+              <Nav.Link href="/guide">이용가이드</Nav.Link>
+              <Nav.Link href="/mypage">마이페이지</Nav.Link>
+              <Nav.Link href="/login">로그인</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      {/* Body */}
+      <main className="flex-grow-1">
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/mypage" element={<MyPage />} />
+          <Route path="/notice" element={<Board />} />
+          <Route path="/qna" element={<Board />} />
+        </Routes>
+      </main>
+
+      {/* Footer */}
+      <footer className="py-4 mt-5 footer bg-dark text-light">
+        <Container>
+          <Row>
+            <Col md={6} className="mb-3 text-center text-md-start mb-md-0">
+              <div>안심누리</div>
+              <div>© CompanyName. All rights reserved.</div>
+            </Col>
+            <Col md={6} className="text-center text-md-end">
+              <Nav className="justify-content-center justify-content-md-end">
+                <Nav.Link className="text-light" href="/guide">
+                  이용가이드
+                </Nav.Link>
+                <Nav.Link className="text-light" href="/download">
+                  앱 다운로드
+                </Nav.Link>
+                <Nav.Link className="text-light" href="/notice">
+                  공지사항
+                </Nav.Link>
+                <Nav.Link className="text-light" href="/qna">
+                  QNA
+                </Nav.Link>
+              </Nav>
+            </Col>
+          </Row>
+        </Container>
+      </footer>
     </Container>
   )
 }
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App'
 import reportWebVitals from './reportWebVitals'
 import '@fontsource/material-icons'
 import {BrowserRouter} from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
