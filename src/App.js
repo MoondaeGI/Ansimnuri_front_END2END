ee5db210e62ccf1ddf82b321924d87edf8d6db0a
test <test@test.com>
2025-05-08 11:27:54 +0900

    add router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css'
 import {Detail, List, Write} from './page/board'
-import Main from './page/main'
+import Main from './page/main/Main'
+import {Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">헤더</header>
-      <main>바디</main>
+      <main>
+        바디
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/notice" element={<List />} />
+        </Routes>
+      </main>
       <footer>푸터</footer>
     </div>
   )
--- src/page/main/component/Map.js
+++ src/page/main/component/Map.js
@@ -1,3 +1,3 @@
-const Map = () => {
+export const Map = () => {
   return <></>
 }
