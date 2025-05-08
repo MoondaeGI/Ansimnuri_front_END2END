a22c72cfaf3204f034c72ca4820f8d4f89bd5083
test <test@test.com>
2025-05-08 11:53:42 +0900

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
 import './App.css'
 import Main from './page/main/Main'
 import Board from './page/board/Board'
+import {Login, MyPage, SignIn} from './page/member'
 import {Routes, Route} from 'react-router-dom'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">헤더</header>
-      <main>
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/notice" element={<Board />} />
-          <Route path="/qna" element={<Board />} />
-        </Routes>
-      </main>
-      <footer>푸터</footer>
-    </div>
+    <Container className="App" fluid>
+      <Row>
+        <Col className="">헤더</Col>
+      </Row>
+      <Row>
+        <main>
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/mypage" element={<MyPage />} />
+            <Route path="/notice" element={<Board />} />
+            <Route path="/qna" element={<Board />} />
+          </Routes>
+        </main>
+      </Row>
+      <Row>
+        <Col>푸터</Col>
+      </Row>
+    </Container>
   )
 }
 
---
+++ src/page/member/SignIn.js
@@ -0,0 +1,3 @@
+export const SignIn = () => {
+  return <div>회원가입</div>
+}
