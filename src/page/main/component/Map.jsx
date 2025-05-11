edcc81c69be77690749e8662f640e64b8b302eaa
MoondaeGI <ansgur119977@gmail.com>
2025-05-12 00:48:56 +0900

    add note

--- src/page/main/component/Map.jsx
+++ src/page/main/component/Map.jsx
@@ -1,3 +1,17 @@
+import {Note, NoteReply} from "../../../component";
+import {useEffect, useState, useMemo} from "react";
+import axios from "axios";
+
 export const Map = () => {
+  const [noteList, setNoteList] = useState([])
+
+  useEffect(() => {
+    axios.get("http://localhost:80/api/note")
+        .then(res => {
+          console.log(res)
+          setNoteList(res.data);
+        })
+  }, [])
+
   return <div>지도</div>
 }
