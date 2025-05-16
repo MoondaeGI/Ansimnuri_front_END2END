8c22ed98ea099c1897e31601bdc9fe31e5fc1108
inho <yuho93@naver.com>
2025-05-16 13:38:32 +0900

    news hide

--- src/page/main/Main.jsx
+++ src/page/main/Main.jsx
@@ -3,30 +3,44 @@ import { Chat, ChatBot, Map, News } from './component';
 import './component/css/main.css';
 
 const Main = () => {
- const [isChatBotOpen, setIsChatBotOpen] = useState(false);
-  const [searchText, setSearchText] = useState('');
+  const [isChatBotOpen, setIsChatBotOpen] = useState(false);
+  const [isNewsOpen, setIsNewsOpen] = useState(false); // 뉴스 토글 상태 추가
 
   const toggleChatBot = () => {
     setIsChatBotOpen(prev => !prev);
   };
+
+  const toggleNews = () => {
+    setIsNewsOpen(prev => !prev);
+  };
+
   return (
     <div className="mainContainer">
       <div className="mainContent">
-        <div className="chatListPanel">
-          <div className="newsBox">
-            <p>뉴스</p>
-          </div>
-
-          <div className="newsList">
-            <News />
-          </div>
-        </div>
+          <button className="newsToggleBtn" onClick={toggleNews}>
+            📰
+          </button>
+          {isNewsOpen && (
+            <div className="newsPanel">
+              <div className="newsBox">
+                <div className="newsHeader">
+                  <span>뉴스</span>
+                  <button onClick={toggleNews}>✕</button>
+                </div>
+              </div>
+              <div className="newsList">
+                <News />
+              </div>
+            </div>
+          )}
+      
 
         <div className="mapSection">
           <div className="mapContainer">
             <Map />
           </div>
         </div>
+
         <button className="chatbotBtn" onClick={toggleChatBot}>
           💬
         </button>
@@ -44,10 +58,7 @@ const Main = () => {
           </div>
         )}
       </div>
-
-
     </div>
-
   );
 };
 
